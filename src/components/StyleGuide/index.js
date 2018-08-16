60dee45fe3508ea1ac13ce8265874758623699eb
LBiddiscombe <lee.biddiscombe@btinternet.com>
2018-08-16 16:50:00 +0100

    tweaked styles

--- src/components/StyleGuide/index.js
+++ src/components/StyleGuide/index.js
@@ -1,12 +1,17 @@
-import React, { Component } from "react";
-import STYLEGUIDE_HTML from "../../assets/styleguide";
+import React, { Component } from 'react'
+import STYLEGUIDE_HTML from '../../assets/styleguide'
 
 export default class StyleGuide extends Component {
   render() {
     return (
       <div className="stylecontainer">
-        <iframe id="styleguide1" title="styleguide" srcDoc={STYLEGUIDE_HTML} />
+        <iframe
+          id="styleguide1"
+          title="styleguide"
+          srcDoc={STYLEGUIDE_HTML}
+          style={{ border: 'none' }}
+        />
       </div>
-    );
+    )
   }
 }
